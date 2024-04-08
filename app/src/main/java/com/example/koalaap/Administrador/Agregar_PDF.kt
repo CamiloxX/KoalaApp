ce568bc274aabc4f26d07c685f14e332d893b13c
Camilo <camisbarragan2425@gmail.com>
2024-04-07 20:44:56 -0500

    Se agrego el fragmento de subir pdf en el dashboard v2

---
+++ app/src/main/java/com/example/koalaap/Administrador/Agregar_PDF.kt
@@ -0,0 +1,12 @@
+package com.example.koalaap.Administrador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.koalaap.R
+
+class Agregar_PDF : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_pdf)
+    }
+}

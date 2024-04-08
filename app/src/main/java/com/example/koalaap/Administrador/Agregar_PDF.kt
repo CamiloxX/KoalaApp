2c91616be343e73591f7bc337f1f34bf1ea74800
Camilo <camisbarragan2425@gmail.com>
2024-04-07 23:16:09 -0500

    Se agrego listar categorias al subir archivo V2

--- app/src/main/java/com/example/koalaap/Administrador/Agregar_PDF.kt
+++ app/src/main/java/com/example/koalaap/Administrador/Agregar_PDF.kt
@@ -1,53 +1,90 @@
 package com.example.koalaap.Administrador
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
 import com.example.koalaap.R
 import com.example.koalaap.databinding.ActivityAgregarPdfBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.koalaap.Elegir_Rol
+
 
 class Agregar_PDF : AppCompatActivity() {
-
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
     private lateinit var binding: ActivityAgregarPdfBinding
-    private  lateinit var categoriaArrayList: ArrayList<ModeloCategoria>
+    private lateinit var categoriaArrayList: ArrayList<ModeloCategoria>
+    private var pdfUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgregarPdfBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
         CargarCategorias()
 
-        binding.IbRegresar.setOnClickListener{
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        binding.IbRegresar.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
 
-        binding.TvCategoriaLibro.setOnClickListener{
+        binding.AdjuntarPdfIb.setOnClickListener {
+            ElegirPdf()
+        }
+
+        binding.TvCategoriaLibro.setOnClickListener {
             SeleccionarCat()
         }
+
+        binding.BtnSubirPdf.setOnClickListener {
+             ValidarInformacion()
         }
-        private fun CargarCategorias(){
+    }
+    private var titulo = ""
+    private var descripcion =""
+    private var categoria = ""
+    private fun ValidarInformacion() {
+        titulo= binding.EtPdf.text.toString().trim()
+
+
+    }
+
+    private fun CargarCategorias() {
         categoriaArrayList = ArrayList()
-            val ref= FirebaseDatabase.getInstance().getReference("Categorías").orderByChild("categoria")
-            ref.addValueEventListener(object : ValueEventListener{
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    categoriaArrayList.clear()
-                    for(ds in snapshot.children){
-                        val modelo = ds.getValue(ModeloCategoria::class.java)
-                        categoriaArrayList.add(modelo!!)
-                    }
+        val ref =
+            FirebaseDatabase.getInstance().getReference("Categorías").orderByChild("categoria")
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                categoriaArrayList.clear()
+                for (ds in snapshot.children) {
+                    val modelo = ds.getValue(ModeloCategoria::class.java)
+                    categoriaArrayList.add(modelo!!)
                 }
+            }
 
-                override fun onCancelled(error: DatabaseError) {
-                    TODO("Not yet implemented")
-                }
-            })
-        }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+    }
 
-    private  var id_categoria = ""
+    private var id_categoria = ""
     private var titulo_categoria = ""
     private fun SeleccionarCat() {
         val categoriasArray = arrayOfNulls<String>(categoriaArrayList.size)
@@ -66,4 +103,28 @@ class Agregar_PDF : AppCompatActivity() {
             }
             .show()
     }
+
+    private fun ElegirPdf() {
+        val intent = Intent()
+        intent.type = "application/pdf"
+        intent.action = Intent.ACTION_GET_CONTENT
+        pdfActivityRL.launch(intent)
+
+
+    }
+
+    private val pdfActivityRL = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult(),
+        ActivityResultCallback<ActivityResult> { resultado ->
+            if (resultado.resultCode == RESULT_OK) {
+                pdfUri = resultado.data!!.data
+            } else {
+                Toast.makeText(this, "Cancelando", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    )
 }
+
+
+

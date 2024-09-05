fbc17f711ffdcfe9c1b04944f159740e1a90d567
Ludwig Alvarado Becerra <70478991+TheLudway@users.noreply.github.com>
2024-09-05 17:33:47 -0500

    KoalaApp 2024

--- app/src/main/java/com/example/koalaap/Bienvenida.kt
+++ app/src/main/java/com/example/koalaap/Bienvenida.kt
@@ -1,3 +1,19 @@
+/*
+    Copyright (C) 2024 Barragán, Tovar & Rodríguez.
+
+    This file is part of KoalaApp.
+
+    KoalaApp is free software: you can redistribute it and/or modify it under the terms of the
+    GNU General Public License as published by the Free Software Foundation, either version 3 of
+    the License, or any later version.
+
+    KoalaApp is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+    without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+    the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along with this program. If
+    not, see <https://www.gnu.org/licenses/>.
+ */
 package com.example.koalaap
 
 import android.content.Intent
